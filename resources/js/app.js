3f0bb9fff216a2be407a99e671141251dcb6c77c
Destrifer <destrifer@yandex.ru>
2024-10-09 15:59:57 +0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'; // Импортируйте createApp из 'vue'
+import './bootstrap';
+import App from './App.vue';
+
+createApp(App).mount("#app"); // Убедитесь, что селектор правильный, используйте "#app" для указания id
